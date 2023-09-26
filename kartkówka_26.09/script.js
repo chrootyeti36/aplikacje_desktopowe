15311d5076c2b94f36e7fdfd6ca68f32d2bee6ef
chrootyeti36 <maciejchrapek2004@gmail.com>
2023-09-26 09:40:16 +0200

    kartkowka

---
+++ kartkówka_26.09/script.js
@@ -0,0 +1,6 @@
+function wypisz(){
+    let imie_nazwisko = document.getElementById("imieNazwisko").value
+    document.getElementById("outputImieNazwisko").innerHTML=imie_nazwisko
+    let ocena = document.getElementById("ocena").value
+    document.getElementById("outputOcena").innerHTML=ocena;
+}
