095e3683a02c8c558fec96fe4246c7baf39fbcea
chrootyeti36 <maciejchrapek2004@gmail.com>
2023-10-03 09:33:50 +0200

    lekcja

--- zajecia_23.10.03/app.js
+++ zajecia_23.10.03/app.js
@@ -1,10 +1,14 @@
 var licznik = 1;
 function numbers(nazwa) {
-    document.getElementById(nazwa).innerHTML = licznik;
-    if(licznik%2==0){
-        document.getElementById(nazwa).style.backgroundColor="red";
-    } else {
-        document.getElementById(nazwa).style.backgroundColor="blue";
+    cell = document.getElementById(nazwa);
+    if (!cell.innerHTML) {
+        document.getElementById(nazwa).innerHTML = licznik;
+        licznik++;
+
+        if (licznik % 2 == 0) {
+            document.getElementById(nazwa).style.backgroundColor = "red";
+        } else {
+            document.getElementById(nazwa).style.backgroundColor = "blue";
+        }
     }
-    licznik++;
 }
