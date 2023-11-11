cfb0a3c93d7e323a9fe1d62cdf75ed77b4a21ea5
chrootpanyeti36 <maciejchrapek2004@gmail.com>
2023-11-11 16:41:56 +0100

    input`y działają

--- projekt_karta_kredytowa/script.js
+++ projekt_karta_kredytowa/script.js
@@ -1,8 +1,53 @@
-function showData(){
-    const cardNumber = document.getElementById("cardNumber")
-    cardNumber.innerHTML = document.getElementById("number").value;
-    console.log(cardNumber);
+//zmienne z formularza
+const number = document.querySelector("#number");
+const holder = document.querySelector("#holder");
+const exDate = document.querySelector("#exDate");
+const CVC = document.querySelector("#CVC");
 
-    const cardHolder = document.getElementById("cardHolder")
-    cardHolder.innerHTML = document.getElementById("holder").value
-}
+//dane wyświetlane na karcie 
+const cardNumber = document.querySelector("#cardNumber");
+const cardHolder = document.querySelector("#cardHolder");
+const cardExDate = document.querySelector("#cardExDate");
+const cardCVC = document.querySelector("#cardCVC");
+
+number.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardNumber.innerText = "#### #### #### ####";
+    } else {
+
+        const input = e.target.value.replaceAll(" - ", "")
+        if (e.target.value.length > 18) {
+            e.target.value = input.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 - $2 - $3 - $4");
+            cardNumber.innerHTML = input.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+        } else if (e.target.value.length > 11) {
+            e.target.value = input.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 - $2 - $3");
+            cardNumber.innerHTML = input.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+        } else if (e.target.value.length > 4) {
+            e.target.value = input.replace(/(\d{4})(\d{0,4})/, "$1 - $2");
+            cardNumber.innerHTML = input.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+        } else {
+            cardNumber.innerHTML = input;
+        }
+    }
+})
+
+holder.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolder.innerHTML = "IMIE NAZWISKO";
+    } else {
+        cardHolder.innerHTML = e.target.value.toUpperCase();
+    }
+})
+
+exDate.addEventListener("keyup", (e) => {
+    const input = e.target.value.replace("/","");
+    console.log(exDate.value);
+})
+
+CVC.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolder.innerHTML = "CVC";
+    } else {
+        cardCVC.innerHTML = e.target.value;
+    }
+})
