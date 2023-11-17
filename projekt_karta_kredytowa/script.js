8557731c6b912031b1184c08f3e041740feea29f
chrootpanyeti36 <maciejchrapek2004@gmail.com>
2023-11-17 20:13:57 +0100

    css do dopracowania

--- projekt_karta_kredytowa/script.js
+++ projekt_karta_kredytowa/script.js
@@ -2,8 +2,7 @@
 const number = document.querySelector("#number");
 const holder = document.querySelector("#holder");
 // const exDate = document.querySelector("#exDate");
-const exMonth = document.querySelector("#exMonth");
-const exYear = document.querySelector("#exYear");
+
 const CVC = document.querySelector("#CVC");
 
 //dane wyświetlane na karcie 
@@ -41,10 +40,6 @@ holder.addEventListener("keyup", (e) => {
     }
 })
 
-// exMonth.addEventListener("change", (e) => {
-//     console.log(exYear);
-// })
-
 CVC.addEventListener("keyup", (e) => {
     if (!e.target.value) {
         cardHolder.innerHTML = "CVC";
@@ -53,54 +48,68 @@ CVC.addEventListener("keyup", (e) => {
     }
 })
 
-function cardBackgroundImg(cover){
-    document.querySelector(".cardFace--front").style.backgroundColor=""
-    document.querySelector(".cardFace--front").style.backgroundImage=cover
+function cardBackgroundImg(cover) {
+    document.querySelector(".cardFace--front").style.backgroundColor = ""
+    document.querySelector(".cardFace--front").style.backgroundImage = cover
 }
 
-document.querySelector("#cardColor").addEventListener("change",(e)=>{
-    document.querySelector(".cardFace--front").style.backgroundImage=""
-    document.querySelector(".cardFace--front").style.backgroundColor=e.target.value
+document.querySelector("#cardColor").addEventListener('change', (e) => {
+    document.querySelector(".cardFace--front").style.backgroundImage = ""
+    document.querySelector(".cardFace--front").style.backgroundColor = e.target.value
 })
 
-document.querySelector(".card").addEventListener('click',function(){
+document.querySelector(".card").addEventListener('click', function () {
     document.querySelector(".card").classList.toggle('is-flipped');
 })
 
-function expiryDate() {
-    if (exMonth.value.length < 2) {
-        cardExMonth = "0" + String(exMonth.value)
+document.querySelector("#exDate").addEventListener('change', (e) => {
+    let year = e.target.value.substring(2, 4);
+    let month = e.target.value.substring(5, 7);
+    if (year && month) {
+        cardExDate.innerHTML = month + "/" + year;
     } else {
-        cardExMonth = String(exMonth.value);
+        cardExDate.innerHTML = "MM/RR";
     }
-    cardExYear = exYear.value;
-    cardExDate.innerHTML = cardExMonth + "/" + cardExYear;
-    console.log(cardExDate)
-}
+    console.log(month, year);
 
-function generateMonth() {
+})
 
-    for (let i = 1; i <= 12; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
-        exMonth.appendChild(option)
-    }
-}
 
-function generateYear() {
-    var today = new Date().getFullYear();
-    var lastTwo = String(today).slice(-2)
-    var year = Number(lastTwo);
-    console.log(year);
 
-    for (let i = year; i <= year + 11; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
-        exYear.appendChild(option);
-    }
-}
+// function generateMonth() {
 
-generateMonth();
-generateYear();
+//     for (let i = 1; i <= 12; i++) {
+//         const option = document.createElement("option");
+//         option.value = i;
+//         option.innerHTML = i;
+//         exMonth.appendChild(option)
+//     }
+// }
+// function generateYear() {
+//     var today = new Date().getFullYear();
+//     var lastTwo = String(today).slice(-2)
+//     var year = Number(lastTwo);
+//     console.log(year);
+
+//     for (let i = year; i <= year + 11; i++) {
+//         const option = document.createElement("option");
+//         option.value = i;
+//         option.innerHTML = i;
+//         exYear.appendChild(option);
+//     }
+// }
+
+// const exMonth = document.querySelector("#exMonth");
+// const exYear = document.querySelector("#exYear");
+// function expiryDate() {
+//     if (exMonth.value.length < 2) {
+//         cardExMonth = "0" + String(exMonth.value)
+//     } else {
+//         cardExMonth = String(exMonth.value);
+//     }
+//     cardExYear = exYear.value;
+//     cardExDate.innerHTML = cardExMonth + "/" + cardExYear;
+//     console.log(cardExDate)
+// }
+// generateMonth();
+// generateYear();
